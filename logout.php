2a1a534f5239b2aafb0c5eaf1594cdc61ac44eb9
“DedRec” <seif.kholy@gmail.com>
2023-01-03 01:16:19 +0200

    Login and Sign up Pages added

---
+++ logout.php
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+<?php 
+require "php/utils.php";
+session_start();
+
+
+session_destroy();
+redirect("/");
+
+=======
+<?php 
+require "php/utils.php";
+session_start();
+
+
+session_destroy();
+redirect("/");
+
+>>>>>>> 3906696ea2a75d289c2acace743a516dc63710df
+?>
