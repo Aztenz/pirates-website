757e2adbe3f50678799193a5a8afa60723973887
“DedRec” <seif.kholy@gmail.com>
2023-01-03 00:40:57 +0200

    Final Commit

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <?php require("php/sessions.php"); 
@@ -38,4 +39,46 @@ require "php/strings.php";?>
 
     
 </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<?php require("php/sessions.php"); 
+require "php/strings.php";?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $logintitle;?></title>
+    <link rel="stylesheet" href="styles/users.css"/>
+</head>
+<body>
+    <div id="left"></div>
+    <div id="right">
+        <form action="php/login.php" method="post">
+            <div class="field">
+                <h1><?php echo $login;?></h1>
+            </div>
+            <div class="field">
+                <input type="text" name="username" placeholder="<?php echo $usernameph;?>" required/>
+            </div>
+            <div class="field">
+                <input type="password" name="password" placeholder="<?php echo $passwordph;?>" required/>
+                
+            </div>
+            <div class="field">
+                <input id type="submit" name="submit"/>
+            </div>
+
+            <div class="field">
+                <a href="/signup.php"> OR SIGN UP </a>
+            </div>
+
+        </form>
+    </div>
+    
+
+    
+</body>
+>>>>>>> 3906696ea2a75d289c2acace743a516dc63710df
 </html>
--- logout.php
+++ logout.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php 
 require "php/utils.php";
 session_start();
@@ -6,4 +7,14 @@ session_start();
 session_destroy();
 redirect("/");
 
+=======
+<?php 
+require "php/utils.php";
+session_start();
+
+
+session_destroy();
+redirect("/");
+
+>>>>>>> 3906696ea2a75d289c2acace743a516dc63710df
 ?>
